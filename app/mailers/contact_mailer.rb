a4ab4aadd76cc64f5e9d65ac9488f561f598fed6
Renato Rosa Franco <renato_ny@live.com>
2019-07-24 11:28:01 -0300

    Contact mailer class added

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class ContactMailer < ApplicationMailer
+
+  default from: 'contato@adotaai.com'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.sended.subject
+  #
+  def sended(contact)
+    @contact = contact
+    mail({
+        to: 'contato@adotaai.com',
+        subject: "Novo e-mail enviado por #{@contact.name}"
+         })
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.received.subject
+  #
+  def received(contact)
+    @contact = contact
+    mail({
+        to: contact.email,
+        subject: "#{@contact.name}, Recebemos seu e-mail com sucesso"
+         })
+  end
+end
