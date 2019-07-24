9fce0878796a4ba9e69d4d66b215a3ba0165a0cb
Renato Rosa Franco <renato_ny@live.com>
2019-07-24 11:15:07 -0300

    Mailer interface added

---
+++ app/models/concerns/mailer_interfaceable.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal
+
+module MailerInterfaceable
+  include ActiveSupport::Concern
+
+  def send_email(mailer, method, object, deliver_method = :deliver_now)
+    mailer.send(method, object).send(deliver_method)
+  end
+end
