6f6f3370ac7be63f401706c540ac44a3cd6e73be
Renato Rosa Franco <renato_ny@live.com>
2019-07-24 11:58:19 -0300

    Updated newsletter mailer class

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,14 +1,19 @@
+# frozen_string_literal: true
+
 class NewsletterMailer < ApplicationMailer
 
+  default from: 'no-reply@adotaai.com'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.newsletter_mailer.signup.subject
   #
-  def signup
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def signup(newsletter)
+    @newsletter = newsletter
+    mail({
+        to: @newsletter.email,
+        subject: "#{@newsletter.name}, Você se inscreveu com sucesso."
+         })
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -16,9 +21,11 @@ class NewsletterMailer < ApplicationMailer
   #
   #   en.newsletter_mailer.cancel.subject
   #
-  def cancel
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def cancel(newsletter)
+    @newsletter = newsletter
+    mail({
+        to: @newsletter.email,
+        subject: "#{@newsletter.name}, que pena, espero te-lo conosco novamente."
+         })
   end
 end
